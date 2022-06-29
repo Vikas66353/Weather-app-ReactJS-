5de773b9f6348e1ce6643b1f62cb797e74fe9130
Vikas Mishra <vm66353@gmail.com>
2022-06-30 04:32:07 +0530

    version 1

---
+++ src/Components/Weather.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { useState, useEffect,useContext } from "react";
+import WeatherCard from "./WeatherCard";
+import Forecast from "./Forecast";
+
+const AppProvider = React.createContext();
+
+const currentURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=bengaluru&appid=47c54d65d5acfce756053187f746677b";
+const forecastURL =
+  "http://api.weatherapi.com/v1/forecast.json?key=19f3fcf03e554ecea42105200222906&q=bengaluru&days=6&aqi=no&alerts=no";
+
+
+const Weather = () => {
+  const [loading, setLoading] = useState(true);
+  const [currentWeatherData, setCurrentWeatherData] = useState(null);
+  const [forecastWeatherData, setForecastWeatherData] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      var foreData = await fetch(forecastURL, {
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*'
+        }}).then((res) => {
+        setLoading(false);
+        return res.json();
+      });
+      var curData = await fetch(currentURL).then((res) => {
+        setLoading(false);
+        return res.json();
+      });
+      setCurrentWeatherData(curData);
+      setForecastWeatherData(foreData.forecast.forecastday);
+      console.log(forecastWeatherData[0].date)
+    }
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <AppProvider.Provider value={forecastWeatherData}>
+       <h1>Weather Report</h1>
+      <div className="container">
+        <div className="weather-details">
+          {currentWeatherData && (
+            <WeatherCard currentWeatherData={currentWeatherData} />
+          )}
+          {forecastWeatherData && <Forecast forecastWeatherData={forecastWeatherData}/>}
+          
+        </div>
+      </div>
+    </AppProvider.Provider>
+  );
+};
+
+export default Weather;
+
+export {AppProvider}
---
+++ src/Components/WeatherCard.js
@@ -0,0 +1,56 @@
+const WeatherCard = ({ currentWeatherData }) => {
+  return (
+    <>
+
+          <div className="current-details">
+            <div className="current-temp">
+              <h1>Lucknow</h1>
+              <h2>
+                {currentWeatherData.main.temp}
+                <sup>o</sup>
+              </h2>
+              <h2>{currentWeatherData.weather[0].main}</h2>
+            </div>
+            <div className="current-weather-details">
+              <div>
+                <span>Humidity</span>
+                <span>{currentWeatherData.main.humidity}</span>
+              </div>
+              <div>
+                <span>Wind</span>
+                <span>
+                  {currentWeatherData.wind.speed},{currentWeatherData.wind.deg}
+                  <span>&#176;</span>{" "}
+                </span>
+              </div>
+              <div>
+                <span>Visibility</span>
+                <span>{currentWeatherData.visibility}</span>
+              </div>
+              <div>
+                <span>Max Temp.</span>
+                <span>{currentWeatherData.main.temp_max}<sup>o</sup></span>
+              </div>
+              <div>
+                <span>Min Temp.</span>
+                <span>{currentWeatherData.main.temp_min}<sup>o</sup></span>
+                
+              </div>
+              <div>
+                <span>Pressure</span>
+                <span>{currentWeatherData.main.pressure}</span>
+              </div>
+              <div>
+                <span>Sunrise Time</span>
+                <span>{currentWeatherData.sys.sunrise}</span>
+              </div>
+              <div>
+                <span>Sunset Time</span>
+                <span>{currentWeatherData.sys.sunset}</span>
+              </div>
+            </div>
+          </div>
+    </>
+  );
+};
+export default WeatherCard;
